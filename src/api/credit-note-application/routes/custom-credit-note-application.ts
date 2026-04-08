baa29a10285d2edd29af82e20daa2dd7f007b5dd
Carlos-Hung-Feng <carloshung1998@gmail.com>
2026-04-08 17:49:29 -0400

    add custom controllers and routes

---
+++ src/api/credit-note-application/routes/custom-credit-note-application.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    {
+      method: 'POST',
+      path: '/credit-note-applications/apply',
+      handler: 'credit-note-application.apply',
+    },
+  ],
+};
---
+++ src/api/credit-note-application/services/credit-note-application.ts
@@ -0,0 +1,7 @@
+/**
+ * credit-note-application service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::credit-note-application.credit-note-application');
